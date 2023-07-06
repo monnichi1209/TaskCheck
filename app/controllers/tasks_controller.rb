fe23c1b6bbc1cd4a2bd7fd793dcfa427b6a52535
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-07-06 22:11:52 +0900

    Implement task status feature with dropdown menu

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,6 +48,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :expired_at)
+    params.require(:task).permit(:name, :description, :expired_at, :status)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
+  STATUS = ['未着手', '着手中', '完了'].freeze
+
+  validates :status, inclusion: { in: Task::STATUS }
 end
