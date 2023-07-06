2e6f315bfdcf0415153b35cd231a03e50b71958a
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-07-06 18:19:20 +0900

    Add initial Task model system tests

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'task[name]', with: 'New Task'
         fill_in 'task[description]', with: 'New Task Content'
+        fill_in 'task[expired_at]', with: DateTime.now
         find('.actions input[type="submit"]').click
 
   
@@ -22,9 +23,22 @@ RSpec.describe 'タスク管理機能', type: :system do
       FactoryBot.create(:task, name: 'task')
       visit tasks_path
       expect(page).to have_content 'task'
-       end
-     end
+      end
+    end
+
+  context '終了期限でソートするというリンクを押した場合' do
+    it '終了期限の降順に並び替えられたタスク一覧が表示される' do
+      task1 = FactoryBot.create(:task, name: 'task1', expired_at: DateTime.now + 1.day)
+      task2 = FactoryBot.create(:task, name: 'task2', expired_at: DateTime.now + 2.days)
+      
+      visit tasks_path(sort_expired: "true")
+      tasks = all('.task_row') 
+
+      expect(tasks[0]).to have_content 'task2'
+      expect(tasks[1]).to have_content 'task1'
+    end
   end
+end
   
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
@@ -38,5 +52,4 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
-  
 end
