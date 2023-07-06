d14f582d435a3fa453a9527bd2d89e7d1318e3dd
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-07-06 17:49:41 +0900

    Add expired_at field to the task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :expired_at)
   end
 end
