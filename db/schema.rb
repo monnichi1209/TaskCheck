e8be3e1486279e29c8a0c8fee0329296e435a8b9
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-07-07 14:43:32 +0900

    Add index to tasks on name, status, and expired_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_06_124031) do
+ActiveRecord::Schema.define(version: 2023_07_07_054216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2023_07_06_124031) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "expired_at", default: -> { "CURRENT_TIMESTAMP" }
     t.string "status"
+    t.index ["expired_at"], name: "index_tasks_on_expired_at"
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
