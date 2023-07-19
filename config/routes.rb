5600902c0027d41fb9d41d9c953e2343a5a541d1
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-07-19 14:16:23 +0900

    ユーザー新規登録・ログイン・ログアウトの実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :tasks
+  resources :users, only: [:new, :create]
+
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
   root to: 'tasks#index'
 end
