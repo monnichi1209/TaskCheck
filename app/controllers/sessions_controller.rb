18c40b71291f11b817fcdd6b0248a07bc362ad76
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-07-22 23:14:45 +0900

    管理者画面熟知

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tasks_path, notice: 'Logged in!'
+      if user.admin?
+        redirect_to admin_users_path, notice: 'Logged in as Admin!'
+      else
+        redirect_to tasks_path, notice: 'Logged in!'
+      end
     else
       flash.now[:alert] = 'Invalid email/password combination'
       render :new
     end
   end
+  
 
   def destroy
     session.delete(:user_id)
